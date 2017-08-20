822c9b7429766b149b26fde52247e24d4ec25cc2
Jessica Kam <jessica.c.kam@gmail.com>
2017-08-19 23:16:58 -0700

    YAS sending messages now work with subscribers and unsubscribers

--- send_sms.py
+++ send_sms.py
@@ -24,12 +24,6 @@ class SMSMessenger():
         self.updateMasterList()
         self.getMsg()
         self.sendMsgs()
-        """
-        for number in self.master_list:
-            print('number: {0}'.format(self.master_list))
-            self.SEND_TO = number
-            self.sendMsg()
-        """
 
     def updateMasterList(self):
         self.saveOldMasterList()
@@ -40,15 +34,12 @@ class SMSMessenger():
     def saveOldMasterList(self):
         print('Saving old master list...')
         self.path = SMSMessenger.WEB_NUMBERS_LOCATION
-        # save current master list, assuming numbers in master list are correctly formatted
-        #if not os.path.exists(SMSMessenger.MASTER_LIST_NUMBERS):
-        #    os.makedirs(SMSMessenger.MASTER_LIST_NUMBERS)
         with open(SMSMessenger.MASTER_LIST_NUMBERS, 'r') as f:
             for line in f:
                 stripped_number = line.replace('\n', '')
                 if stripped_number:
+                    print(stripped_number)
                     self.master_list.append(stripped_number)
-            #self.master_lst = master_list.readlines()
         print(self.master_list)
             
     def addSubscribers(self):
@@ -60,11 +51,8 @@ class SMSMessenger():
                 for line in new_number_file:
                     stripped_number = line.replace('\n', '')
                     if stripped_number and self.parseNumber(stripped_number):
+                        print(stripped_number)
                         self.master_list.append('+1' + self.parseNumber(stripped_number))
-                #phone_number = new_number_file.readline()
-                #parsed_number = self.parseNumber(phone_number)
-                #if parsed_number:
-                #    self.master_lst.append('+1' + phone_number)
             os.remove(file_path)
         print(self.master_list)
             
@@ -77,31 +65,26 @@ class SMSMessenger():
             with open(file_path, 'r') as new_number_file:
                 for line in new_number_file:
                     stripped_number = line.replace('\n', '')
+                    print(stripped_number)
                     if stripped_number and self.parseNumber(stripped_number):
                         number = '+1' + self.parseNumber(stripped_number)
                         lst_to_unsubscribe.append(number)
-                #unsubscribe_number = new_number_file.readline()
-                #parsed_number = self.parseNumber(unsubscribe_number)
-                #if parsed_number:
-                #    lst_to_unsubscribe.append('+1' + unsubscribe_number)
-                
+
                         # also save to master unsubscribe list
                         with open(SMSMessenger.MASTER_LIST_UNSUBSCRIBED, 'a') as f:
                             f.write(number + '\n') 
-                            #f.write('+1' + unsubscribe_number + '\n') 
             os.remove(file_path)
         for unsubscriber in lst_to_unsubscribe:
-            print('unsub: {0}'.format(unsubscriber))
-            if unsubscriber in self.master_lst:
-                self.master_lst.remove(unsubscriber)
+            if unsubscriber in self.master_list:
+                self.master_list.remove(unsubscriber)
         print(self.master_list)
         
     
     def createNewMasterList(self):
         print('Creating a new master list...')
         with open(SMSMessenger.MASTER_LIST_NUMBERS, 'w') as f:
-            for number in self.master_lst:
-                print('num adding to new list: {0}'.format(number))
+            for number in self.master_list:
+                print(number)
                 f.write(number + '\n')
 
     def parseNumber(self, line):
@@ -131,8 +114,8 @@ class SMSMessenger():
         for number in self.master_list:
             self.SEND_TO = number
             self.client.api.account.messages.create(to=self.SEND_TO,
-                                           from_= self.SEND_FROM,
-                                           body=self.msg)
+                                                    from_= self.SEND_FROM,
+                                                    body=self.msg)
             print('Sent msg to {0}'.format(self.SEND_TO))
 
 if __name__=='__main__':
